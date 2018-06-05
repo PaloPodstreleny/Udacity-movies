6693ca32485a59289fd599d017c08886bd0f8f6c
Pavol Podstreleny <palo.podstreleny@gmail.com>
2018-06-05 13:17:59 +0100

    Added menu and menuitems in homepage

--- app/src/main/java/sk/udacity/podstreleny/palo/movie/MainActivity.java
+++ app/src/main/java/sk/udacity/podstreleny/palo/movie/MainActivity.java
@@ -2,6 +2,9 @@ package sk.udacity.podstreleny.palo.movie;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.order_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.top_rating:
+                Toast.makeText(this,"TopRating",Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.pupularity:
+                Toast.makeText(this,"Popularity",Toast.LENGTH_LONG).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
